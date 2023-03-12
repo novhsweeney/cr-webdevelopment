4046a61f3d5358fe0152a9451d8fe10cc278367b
Noah Sweeney <74623979+novhsweeney@users.noreply.github.com>
2023-03-11 22:57:40 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+//selecting html elements
+let emailElement = document.querySelector('#email');
+let messageElement = document.querySelector('#message')
+
+let submitButton = document.querySelector('#submit-button');
+submitButton.addEventListener('click', function(e) {
+	e.preventDefault();
+
+	let emailValue = emailElement.value
+	let messageValue = messageElement.value
+
+	if(emailValue.includes('@')) {
+		//all good
+		alert('Thank you for your message')
+	} else {
+		alert('oh no. That does not look like a valid email address. Please try again')
+	}
+})
+
+
+//attaching 'click listener'
+//getting user entered values
+//javascript validations
